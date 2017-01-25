3ec1a4fa9fb7ced2f9fec4a754ce91e0d3ace206
Daniel Lavoie <dlavoie@live.ca>
2017-01-25 12:16:03 +0100

    Ajout des demos pour Formation Liege

--- demo-spring-boot/validation-service/src/main/java/com/invivoo/spring/cloud/validationservice/ValidationAutoConfiguration.java
+++ demo-spring-boot/validation-service/src/main/java/com/invivoo/spring/cloud/validationservice/ValidationAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.invivoo.spring.cloud.validationservice;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @EntityScan
 @Configuration
